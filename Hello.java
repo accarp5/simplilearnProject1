1f48bb5d99bbe54212235b535d241d13f7449df9
Accarp T <accarp5@gmail.com>
2023-05-04 18:40:40 +0000

    Added added another print statement

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello {
   public static void main(String[] args){
     System.out.println("This is a java app created using Docker for project1");
+    System.out.println("This is another statement added to java app"); 
   }
 }
